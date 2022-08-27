1d404dcedaaba938c965ce12296532b5d0d4dd02
WEN Hao <wenh06@gmail.com>
2022-08-27 17:11:14 +0800

    adjust variable names

--- src/_diff_binom_confint.py
+++ src/_diff_binom_confint.py
@@ -42,62 +42,79 @@ def compute_difference_confidence_interval(
     z = norm.ppf((1 + conf_level) / 2)
     tot = n_positive + n_negative
     ref_tot = ref_positive + ref_negative
-    r1 = n_positive / tot
-    r2 = ref_positive / ref_tot
+    ratio = n_positive / tot
+    ref_ratio = ref_positive / ref_tot
     if confint_type.lower() in ["wilson", "newcombe"]:
         lower1 = (
-            (2 * tot * r1 + z**2 - z * np.sqrt(4 * tot * r1 * (1 - r1) + z**2))
+            (
+                2 * tot * ratio
+                + z**2
+                - z * np.sqrt(4 * tot * ratio * (1 - ratio) + z**2)
+            )
             / 2
             / (tot + z**2)
         )
         upper1 = (
-            (2 * tot * r1 + z**2 + z * np.sqrt(4 * tot * r1 * (1 - r1) + z**2))
+            (
+                2 * tot * ratio
+                + z**2
+                + z * np.sqrt(4 * tot * ratio * (1 - ratio) + z**2)
+            )
             / 2
             / (tot + z**2)
         )
         lower2 = (
             (
-                2 * ref_tot * r2
+                2 * ref_tot * ref_ratio
                 + z**2
-                - z * np.sqrt(4 * ref_tot * r2 * (1 - r2) + z**2)
+                - z * np.sqrt(4 * ref_tot * ref_ratio * (1 - ref_ratio) + z**2)
             )
             / 2
             / (ref_tot + z**2)
         )
         upper2 = (
             (
-                2 * ref_tot * r2
+                2 * ref_tot * ref_ratio
                 + z**2
-                + z * np.sqrt(4 * ref_tot * r2 * (1 - r2) + z**2)
+                + z * np.sqrt(4 * ref_tot * ref_ratio * (1 - ref_ratio) + z**2)
             )
             / 2
             / (ref_tot + z**2)
         )
         return ConfidenceInterval(
-            r1 - r2 - np.sqrt((r1 - lower1) ** 2 + (upper2 - r2) ** 2),
-            r1 - r2 + np.sqrt((r2 - lower2) ** 2 + (upper1 - r1) ** 2),
+            ratio
+            - ref_ratio
+            - np.sqrt((ratio - lower1) ** 2 + (upper2 - ref_ratio) ** 2),
+            ratio
+            - ref_ratio
+            + np.sqrt((ref_ratio - lower2) ** 2 + (upper1 - ratio) ** 2),
             conf_level,
             confint_type.lower(),
         )
     elif confint_type.lower() in ["wilson_cc", "newcombe_cc"]:
         raise NotImplementedError
     elif "wald" in confint_type.lower():
-        item = z * np.sqrt(r1 * (1 - r1) / tot + r2 * (1 - r2) / ref_tot)
+        item = z * np.sqrt(
+            ratio * (1 - ratio) / tot + ref_ratio * (1 - ref_ratio) / ref_tot
+        )
         if confint_type.lower() == "wald_cc":
             return ConfidenceInterval(
-                r1 - r2 - item - 0.5 / tot - 0.5 / ref_tot,
-                r1 - r2 + item + 0.5 / tot + 0.5 / ref_tot,
+                ratio - ref_ratio - item - 0.5 / tot - 0.5 / ref_tot,
+                ratio - ref_ratio + item + 0.5 / tot + 0.5 / ref_tot,
                 conf_level,
                 confint_type.lower(),
             )
         return ConfidenceInterval(
-            r1 - r2 - item, r1 - r2 + item, conf_level, confint_type.lower()
+            ratio - ref_ratio - item,
+            ratio - ref_ratio + item,
+            conf_level,
+            confint_type.lower(),
         )
     elif confint_type.lower() in ["haldane", "jeffreys-perks"]:
         v = 0.25 / tot - 0.25 / ref_tot
         u = v + 0.5 / ref_tot
         if confint_type.lower() == "haldane":
-            psi = 0.5 * (r1 + r2)
+            psi = 0.5 * (ratio + ref_ratio)
         else:
             psi = 0.5 * (
                 (n_positive + 0.5) / (tot + 1) + (ref_positive + 0.5) / (ref_tot + 1)
@@ -107,8 +124,8 @@ def compute_difference_confidence_interval(
                 u
                 * (
                     4 * psi * (1 - psi)
-                    - (r1 - r2) ** 2
-                    + 2 * v * (1 - 2 * psi) * (r1 - r2)
+                    - (ratio - ref_ratio) ** 2
+                    + 2 * v * (1 - 2 * psi) * (ratio - ref_ratio)
                     + 4 * ((z * u) ** 2) * psi * (1 - psi)
                     + (z * v * (1 - 2 * psi)) ** 2
                 )
@@ -116,7 +133,9 @@ def compute_difference_confidence_interval(
             * z
             / (1 + u * z**2)
         )
-        theta_star = ((r1 - r2) + v * (1 - 2 * psi) * z**2) / (1 + u * z**2)
+        theta_star = ((ratio - ref_ratio) + v * (1 - 2 * psi) * z**2) / (
+            1 + u * z**2
+        )
         return ConfidenceInterval(
             theta_star - w, theta_star + w, conf_level, confint_type.lower()
         )
