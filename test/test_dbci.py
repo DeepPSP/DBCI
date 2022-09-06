9fb4bbf6780cdfb510fac8039511e3205ea30b52
WEN Hao <wenh06@gmail.com>
2022-09-06 11:00:34 +0800

    update test functions

--- test/test_dbci.py
+++ test/test_dbci.py
@@ -1,17 +1,49 @@
 """
 """
 
+import re
 from pathlib import Path
+from typing import List
 
+import pandas as pd
 from pytest import approx
 
 try:
     from dbci import compute_difference_confidence_interval
+    from dbci._diff_binom_confint import _supported_types
 except ImportError:
     import sys
 
     sys.path.insert(0, str(Path(__file__).parents[1]))
     from src import compute_difference_confidence_interval
+    from src._diff_binom_confint import _supported_types
+
+
+_TEST_DATA_DIR = Path(__file__).parent / "test-data"
+
+
+def load_test_data() -> List[pd.DataFrame]:
+    test_file_pattern = "example-(?P<n_positive>[\\d]+)-(?P<n_tot>[\\d]+)-vs-(?P<ref_positive>[\\d]+)-(?P<ref_tot>[\\d]+)\\.csv"
+    test_files = Path(_TEST_DATA_DIR).glob("*.csv")
+    test_data = []
+    for file in test_files:
+        match = re.match(test_file_pattern, file.name)
+        if match:
+            n_positive = int(match.group("n_positive"))
+            n_tot = int(match.group("n_tot"))
+            ref_positive = int(match.group("ref_positive"))
+            ref_tot = int(match.group("ref_tot"))
+            df_data = pd.read_csv(file)
+            test_data.append(
+                {
+                    "n_positive": n_positive,
+                    "n_negative": n_tot - n_positive,
+                    "ref_positive": ref_positive,
+                    "ref_negative": ref_tot - ref_positive,
+                    "data": df_data,
+                }
+            )
+    return test_data
 
 
 def test_difference_confidence_interval():
@@ -28,126 +60,29 @@ def test_difference_confidence_interval():
     Reference
     ---------
     Reynes JLA, Pulido F, et al., “Lopinavir/ritonavir combined with raltegravir demonstrated similar antiviral efficacy and safety as lopinavir/ritonavir combined with tenofovir disoproxil fumarate/emtricitabine in treatment-naïve HIV-1 infected subjects.” Program and abstracts of the XVIII International AIDS Conference; July 18-23, 2010; Vienna, Austria. Abstract MOAB0101.
+
     """
     n_positive, n_negative = 84, 101 - 84
     ref_positive, ref_negative = 89, 105 - 89
+    df_data = pd.read_csv(_TEST_DATA_DIR / "example-84-101-vs-89-105.csv")
 
-    # Wald, no CC
-    lower_bound, upper_bound = compute_difference_confidence_interval(
-        n_positive, n_negative, ref_positive, ref_negative, confint_type="wald"
-    ).astuple()
+    max_length = max([len(x) for x in _supported_types]) + 1
 
-    assert lower_bound == approx(-0.1162, abs=1e-4)
-    assert upper_bound == approx(0.0843, abs=1e-4)
+    for confint_type in _supported_types:
+        lower, upper = compute_difference_confidence_interval(
+            n_positive, n_negative, ref_positive, ref_negative, confint_type=confint_type
+        ).astuple()
+        print(f"{confint_type.ljust(max_length)}: [{lower:.2%}, {upper:.2%}]")
+        try:
+            row = df_data[df_data["method"] == confint_type].iloc[0]
+        except IndexError:
+            pass  # alias method, e.g. "wilson" and "newcombe"
+        assert lower == approx(row["lower_bound"], abs=1e-4)
+        assert upper == approx(row["upper_bound"], abs=1e-4)
 
-    # Wald, CC
-    lower_bound, upper_bound = compute_difference_confidence_interval(
-        n_positive, n_negative, ref_positive, ref_negative, confint_type="wald_cc"
-    ).astuple()
-
-    assert lower_bound == approx(-0.1259, abs=1e-4)
-    assert upper_bound == approx(0.0940, abs=1e-4)
-
-    # Haldane
-    lower_bound, upper_bound = compute_difference_confidence_interval(
-        n_positive, n_negative, ref_positive, ref_negative, confint_type="haldane"
-    ).astuple()
-
-    assert lower_bound == approx(-0.1152, abs=1e-4)
-    assert upper_bound == approx(0.0834, abs=1e-4)
-
-    # Jeffreys-Perks
-    lower_bound, upper_bound = compute_difference_confidence_interval(
-        n_positive,
-        n_negative,
-        ref_positive,
-        ref_negative,
-        confint_type="jeffreys-perks",
-    ).astuple()
-
-    assert lower_bound == approx(-0.1160, abs=1e-4)
-    assert upper_bound == approx(0.0843, abs=1e-4)
-
-    # Mee, NOT implemented yet
-    # lower_bound, upper_bound = compute_difference_confidence_interval(
-    #     n_positive, n_negative, ref_positive, ref_negative, confint_type="mee"
-    # ).astuple()
-
-    # assert lower_bound == approx(-0.1188, abs=1e-4)
-    # assert upper_bound == approx(0.0857, abs=1e-4)
-
-    # Miettinen-Nurminen, NOT implemented yet
-    # lower_bound, upper_bound = compute_difference_confidence_interval(
-    #     n_positive,
-    #     n_negative,
-    #     ref_positive,
-    #     ref_negative,
-    #     confint_type="miettinen-nurminen",
-    # ).astuple()
-
-    # assert lower_bound == approx(-0.1191, abs=1e-4)
-    # assert upper_bound == approx(0.0860, abs=1e-4)
-
-    # Wilson, no CC
-    lower_bound, upper_bound = compute_difference_confidence_interval(
-        n_positive, n_negative, ref_positive, ref_negative, confint_type="wilson"
-    ).astuple()
-
-    assert lower_bound == approx(-0.1177, abs=1e-4)
-    assert upper_bound == approx(0.0851, abs=1e-4)
-
-    # Wilson, CC, NOT implemented yet
-    # lower_bound, upper_bound = compute_difference_confidence_interval(
-    #     n_positive, n_negative, ref_positive, ref_negative, confint_type="wilson_cc"
-    # ).astuple()
-
-    # assert lower_bound == approx(-0.1245, abs=1e-4)
-    # assert upper_bound == approx(0.0918, abs=1e-4)
-
-    # True profile, NOT implemented yet
-    # lower_bound, upper_bound = compute_difference_confidence_interval(
-    #     n_positive, n_negative, ref_positive, ref_negative, confint_type="true-profile"
-    # ).astuple()
-
-    # assert lower_bound == approx(-0.1176, abs=1e-4)
-    # assert upper_bound == approx(0.0849, abs=1e-4)
-
-    # Hauck-Andersen, NOT implemented yet
-    # lower_bound, upper_bound = compute_difference_confidence_interval(
-    #     n_positive, n_negative, ref_positive, ref_negative, confint_type="hauck-anderson"
-    # ).astuple()
-
-    # assert lower_bound == approx(-0.1216, abs=1e-4)
-    # assert upper_bound == approx(0.0898, abs=1e-4)
-
-    # Agresti-Caffo, NOT implemented yet
-    # lower_bound, upper_bound = compute_difference_confidence_interval(
-    #     n_positive, n_negative, ref_positive, ref_negative, confint_type="agresti-caffo"
-    # ).astuple()
-
-    # assert lower_bound == approx(-0.1168, abs=1e-4)
-    # assert upper_bound == approx(0.0850, abs=1e-4)
-
-    # Santner-Snell, NOT implemented yet
-    # lower_bound, upper_bound = compute_difference_confidence_interval(
-    #     n_positive, n_negative, ref_positive, ref_negative, confint_type="santner-snell"
-    # ).astuple()
-
-    # assert lower_bound == approx(-0.1514, abs=1e-4)
-    # assert upper_bound == approx(0.1219, abs=1e-4)
-
-    # Chan-Zhang, NOT implemented yet
-    # lower_bound, upper_bound = compute_difference_confidence_interval(
-    #     n_positive, n_negative, ref_positive, ref_negative, confint_type="chan-zhang"
-    # ).astuple()
-
-    # assert lower_bound == approx(-0.1227, abs=1e-4)
-    # assert upper_bound == approx(0.0869, abs=1e-4)
-
-    # Brown-Li, NOT implemented yet
-
-    # Miettinen-Nurminen-Brown-Li, NOT implemented yet
+    print("test_difference_confidence_interval passed")
 
 
 if __name__ == "__main__":
+    load_test_data()
     test_difference_confidence_interval()
